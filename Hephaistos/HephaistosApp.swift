5fd67afa66752be50288a534335ffe534f214acd
Gil <weathering294@gmail.com>
2026-02-17 16:14:53 +0100

    Initial Commit

---
+++ Hephaistos/HephaistosApp.swift
@@ -0,0 +1,17 @@
+//
+//  HephaistosApp.swift
+//  Hephaistos
+//
+//  Created by Gil Rodrigues on 17.02.26.
+//
+
+import SwiftUI
+
+@main
+struct HephaistosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
